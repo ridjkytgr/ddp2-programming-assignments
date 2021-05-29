4b0b709cf4c6eed10a11e99cd62aeb1f6a7a5160
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-05-30 00:28:15 +0800

    Implementasi RingkasanMahasiswaGUI.java beserta DetailRingkasanMahasiswaGUI.java

--- assignment4/src/main/java/assignments/assignment4/frontend/DetailRingkasanMahasiswaGUI.java
+++ assignment4/src/main/java/assignments/assignment4/frontend/DetailRingkasanMahasiswaGUI.java
@@ -9,7 +9,108 @@ import assignments.assignment4.backend.*;
 
 public class DetailRingkasanMahasiswaGUI {
     public DetailRingkasanMahasiswaGUI(JFrame frame, Mahasiswa mahasiswa, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah){
+        JPanel detailRingkasanMhsGUI = new JPanel();
+        // Menambahkan glue agar konten ada di tengah
+        detailRingkasanMhsGUI.add(Box.createVerticalGlue());
 
-        // TODO: Implementasikan Detail Ringkasan Mahasiswa
+        detailRingkasanMhsGUI.setLayout(new BoxLayout(detailRingkasanMhsGUI, BoxLayout.Y_AXIS));
+        detailRingkasanMhsGUI.setBackground(new Color(0xCD7841));
+
+        // Menambahkan label judul
+        JLabel newLabel = new JLabel();
+        newLabel.setText("Detail Ringkasan Mahasiswa");
+        newLabel.setFont(SistemAkademikGUI.fontTitle);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        detailRingkasanMhsGUI.add(newLabel);
+        detailRingkasanMhsGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menampung variabel label-label yang perlu ditampung
+        String nama = mahasiswa.getNama();
+        long npm = mahasiswa.getNpm();
+        String jurusan = mahasiswa.extractJurusan(mahasiswa.getNpm());
+
+        MataKuliah[] matkul = mahasiswa.getMataKuliah();
+        int banyakMatkul = mahasiswa.getBanyakMatkul();
+
+        int sks = mahasiswa.getTotalSKS();
+
+        mahasiswa.cekIRS();
+        String[] masalahIRS = mahasiswa.getMasalahIRS();
+        int banyakMasalahIRS = mahasiswa.getBanyakMasalahIRS();
+
+
+        // Menambahkan label-label yang akan ditampilkan
+        addLabelGeneral(detailRingkasanMhsGUI, "Nama: " + nama);
+        addLabelGeneral(detailRingkasanMhsGUI, "NPM: " + npm);
+        addLabelGeneral(detailRingkasanMhsGUI, "Jurusan: " + jurusan);
+        addLabelGeneral(detailRingkasanMhsGUI, "Daftar Mata Kuliah:");
+
+        // Mencetak matkul-matkul yang diambil oleh mahasiswa
+        for (int i = 0; i < banyakMatkul; i++) {
+            JLabel masalahIRSLabel = new JLabel();
+            masalahIRSLabel.setText(Integer.toString(i+1) + ". " + matkul[i]);
+            masalahIRSLabel.setFont(new Font("Century Gothic", Font.BOLD, 12));
+            masalahIRSLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            detailRingkasanMhsGUI.add(masalahIRSLabel);
+            detailRingkasanMhsGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+        }
+        addLabelGeneral(detailRingkasanMhsGUI, "Total SKS: " + sks);
+        addLabelGeneral(detailRingkasanMhsGUI, "Hasil Pengecekan IRS:");
+
+        // Mencetak hasil pengecekan IRS
+        if (banyakMasalahIRS == 0) {
+            JLabel masalahIRSLabel = new JLabel();
+            masalahIRSLabel.setText("IRS tidak bermasalah.");
+            masalahIRSLabel.setFont(new Font("Century Gothic", Font.BOLD, 12));
+            masalahIRSLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            detailRingkasanMhsGUI.add(masalahIRSLabel);
+            detailRingkasanMhsGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+        } else {
+            for (int i = 0; i < banyakMasalahIRS; i++) {
+                JLabel masalahIRSLabel = new JLabel();
+                masalahIRSLabel.setText(Integer.toString(i + 1) + ". " + masalahIRS[i]);
+                masalahIRSLabel.setFont(new Font("Century Gothic", Font.BOLD, 12));
+                masalahIRSLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                detailRingkasanMhsGUI.add(masalahIRSLabel);
+                detailRingkasanMhsGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+
+        // Menambahkan button Selesai
+        JButton kembaliButton = new JButton("Selesai");
+        kembaliButton.setFocusable(false);
+        kembaliButton.setFont(SistemAkademikGUI.fontGeneral);
+        kembaliButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        kembaliButton.setForeground(new Color(0xFFFFFF));
+        kembaliButton.setBackground(new Color(0x059846));
+        kembaliButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Menghapus panel
+                frame.getContentPane().removeAll();
+                frame.repaint();
+
+                // Pindah ke panel homeGUI
+                new HomeGUI(frame, daftarMahasiswa, daftarMataKuliah);
+            }
+        });
+        detailRingkasanMhsGUI.add(kembaliButton);
+
+        // Menambahkan glue agar konten ada di tengah
+        detailRingkasanMhsGUI.add(Box.createVerticalGlue());
+
+        frame.add(detailRingkasanMhsGUI);
+        frame.setVisible(true);
+
     }
+
+    private void addLabelGeneral(JPanel panel, String text) {
+        JLabel newLabel = new JLabel();
+        newLabel.setText(text);
+        newLabel.setFont(SistemAkademikGUI.fontGeneral);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(newLabel);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+    }
+
 }
--- assignment4/src/main/java/assignments/assignment4/frontend/RingkasanMahasiswaGUI.java
+++ assignment4/src/main/java/assignments/assignment4/frontend/RingkasanMahasiswaGUI.java
@@ -10,13 +10,124 @@ import assignments.assignment4.backend.*;
 public class RingkasanMahasiswaGUI {
 
     public RingkasanMahasiswaGUI(JFrame frame, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah){
+        JPanel ringkasanMahasiswaGUI = new JPanel();
+        // Menambahkan glue agar konten ada di tengah
+        ringkasanMahasiswaGUI.add(Box.createVerticalGlue());
 
-        // TODO: Implementasikan Ringkasan Mahasiswa
+        ringkasanMahasiswaGUI.setLayout(new BoxLayout(ringkasanMahasiswaGUI, BoxLayout.Y_AXIS));
+        ringkasanMahasiswaGUI.setBackground(new Color(0xCD7841));
+
+        // Membuat array yang menyimpan npm dari mahasiswa untuk ditampilkan pada dropdown
+        Long[] arrayNpm = new Long[daftarMahasiswa.size()];
+
+        // Mengisi array arrayNpm dengan NPM dari mahasiswa
+        for (int i = 0; i < daftarMahasiswa.size(); i++) {
+            if (daftarMahasiswa.get(i) != null) {
+                arrayNpm[i] = daftarMahasiswa.get(i).getNpm();
+            } else if (daftarMahasiswa.get(i) == null) {
+                break;
+            }
+        }
+
+        // Melakukan sort terhadap arrayNpm
+        Long[] arrayNpmSorted = sortNpm(arrayNpm);
+
+        // Menambahkan label judul
+        JLabel newLabel = new JLabel();
+        newLabel.setText("Ringkasan Mahasiswa");
+        newLabel.setFont(SistemAkademikGUI.fontTitle);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ringkasanMahasiswaGUI.add(newLabel);
+        ringkasanMahasiswaGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan label Pilih NPM
+        JLabel pilihNpmLabel = new JLabel();
+        pilihNpmLabel.setText("Pilih NPM");
+        pilihNpmLabel.setFont(SistemAkademikGUI.fontGeneral);
+        pilihNpmLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ringkasanMahasiswaGUI.add(pilihNpmLabel);
+        ringkasanMahasiswaGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan dropdownNpm
+        JComboBox<Long> dropdownNpm = new JComboBox<>(arrayNpmSorted);
+        dropdownNpm.setMaximumSize(new Dimension(175, 0));
+        ringkasanMahasiswaGUI.add(dropdownNpm);
+        ringkasanMahasiswaGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan button Lihat
+        JButton lihatButton = new JButton("lihat");
+        lihatButton.setFocusable(false);
+        lihatButton.setFont(SistemAkademikGUI.fontGeneral);
+        lihatButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lihatButton.setForeground(new Color(0xFFFFFF));
+        lihatButton.setBackground(new Color(0x0549B0));
+        lihatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mengambil nilai dari dropdown
+                Long npmSelected = (Long) dropdownNpm.getSelectedItem();
+
+                // Jika terdapat dropdown yang masih kosong
+                if (npmSelected == null) {
+                    JOptionPane.showMessageDialog(frame, "Mohon isi seluruh Field");
+                } else {
+                    // Membuka panel detail ringkasan mahasiswa
+                    Mahasiswa mahasiswa = getMahasiswa(npmSelected, daftarMahasiswa);
+
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    new DetailRingkasanMahasiswaGUI(frame, mahasiswa, daftarMahasiswa, daftarMataKuliah);
+                }
+            }
+        });
+        ringkasanMahasiswaGUI.add(lihatButton);
+        ringkasanMahasiswaGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan button Kembali
+        JButton kembaliButton = new JButton("Kembali");
+        kembaliButton.setFocusable(false);
+        kembaliButton.setFont(SistemAkademikGUI.fontGeneral);
+        kembaliButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        kembaliButton.setForeground(new Color(0xFFFFFF));
+        kembaliButton.setBackground(new Color(0x059846));
+        kembaliButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Menghapus panel
+                frame.getContentPane().removeAll();
+                frame.repaint();
+
+                // Pindah ke panel homeGUI
+                new HomeGUI(frame, daftarMahasiswa, daftarMataKuliah);
+            }
+        });
+        ringkasanMahasiswaGUI.add(kembaliButton);
+
+
+        // Menambahkan glue agar konten ada di tengah
+        ringkasanMahasiswaGUI.add(Box.createVerticalGlue());
+
+        frame.add(ringkasanMahasiswaGUI);
+        frame.setVisible(true);
     }
 
-    // Uncomment method di bawah jika diperlukan
-    /*
-    private Mahasiswa getMahasiswa(long npm) {
+    private Long[] sortNpm(Long[] arrayNpm) {
+        for (int i = 0; i < arrayNpm.length; i++) {
+            for (int j = i + 1; j < arrayNpm.length; j++) {
+                if (arrayNpm[i] > arrayNpm[j]) {
+                    // Melakukan swap
+                    Long tempMin = arrayNpm[j];
+                    arrayNpm[j] = arrayNpm[i];
+                    arrayNpm[i] = tempMin;
+                }
+            }
+        }
+        return arrayNpm;
+    }
+
+    private Mahasiswa getMahasiswa(long npm, ArrayList<Mahasiswa> daftarMahasiswa) {
 
         for (Mahasiswa mahasiswa : daftarMahasiswa) {
             if (mahasiswa.getNpm() == npm){
@@ -25,5 +136,4 @@ public class RingkasanMahasiswaGUI {
         }
         return null;
     }
-    */
 }
