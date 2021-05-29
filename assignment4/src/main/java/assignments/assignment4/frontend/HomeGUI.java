cae829474112a2158dea8612ef9681ba77ef979d
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-05-29 15:19:00 +0800

    Implementasi class HomeGUI.java

--- assignment4/src/main/java/assignments/assignment4/frontend/HomeGUI.java
+++ assignment4/src/main/java/assignments/assignment4/frontend/HomeGUI.java
@@ -11,11 +11,85 @@ import assignments.assignment4.backend.*;
 public class HomeGUI {
     
     public HomeGUI(JFrame frame, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah){
+        JPanel homeGUIPanel = new JPanel();
+        homeGUIPanel.add(Box.createVerticalGlue());
+        homeGUIPanel.setLayout(new BoxLayout(homeGUIPanel, BoxLayout.Y_AXIS));
+        homeGUIPanel.setBackground(new Color(0xCD7841));
+
         JLabel titleLabel = new JLabel();
         titleLabel.setText("Selamat datang di Sistem Akademik");
-        titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setFont(SistemAkademikGUI.fontTitle);
-        
-        // TODO: Implementasikan Halaman Home
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        homeGUIPanel.add(titleLabel);
+        homeGUIPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Tambah Mahasiswa");
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Tambah Mata Kuliah");
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Tambah IRS");
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Hapus IRS");
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Lihat Ringkasan Mahasiswa");
+        addButton(frame, daftarMahasiswa, daftarMataKuliah, homeGUIPanel,"Lihat Ringkasan Mata Kuliah");
+
+        frame.add(homeGUIPanel);
+        homeGUIPanel.add(Box.createVerticalGlue());
+    }
+
+    public void addButton(JFrame frame, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah, JPanel panel, String text) {
+        JButton newButton = new JButton(text);
+        newButton.setFocusable(false);
+        newButton.setFont(SistemAkademikGUI.fontGeneral);
+        newButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        newButton.setForeground(new Color(0xFFFFFF));
+        newButton.setBackground(new Color(0x0549B0));
+        newButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (text.equals("Tambah Mahasiswa")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel tambahMahasiswa
+                    new TambahMahasiswaGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                } else if (text.equals("Tambah Mata Kuliah")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel tambahMataKuliah
+                    new TambahMataKuliahGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                } else if (text.equals("Tambah IRS")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel tambahIRS
+                    new TambahIRSGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                } else if (text.equals("Hapus IRS")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel hapusIRS
+                    new HapusIRSGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                } else if (text.equals("Lihat Ringkasan Mahasiswa")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel hapusIRS
+                    new RingkasanMahasiswaGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                } else if (text.equals("Lihat Ringkasan Mata Kuliah")) {
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    // Pindah ke panel hapusIRS
+                    new RingkasanMataKuliahGUI(frame, daftarMahasiswa, daftarMataKuliah);
+                }
+            }
+        });
+        panel.add(newButton);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
     }
 }
