35698011d0acb641e3bdc02b8c96920c23ca5172
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-05-30 00:28:41 +0800

    Implementasi RingkasanMataKuliahGUI.java beserta DetailRingkasanMataKuliahGUI.java

--- assignment4/src/main/java/assignments/assignment4/frontend/DetailRingkasanMataKuliahGUI.java
+++ assignment4/src/main/java/assignments/assignment4/frontend/DetailRingkasanMataKuliahGUI.java
@@ -9,8 +9,92 @@ import assignments.assignment4.backend.*;
 
 public class DetailRingkasanMataKuliahGUI {
     public DetailRingkasanMataKuliahGUI(JFrame frame, MataKuliah mataKuliah, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah){
+        JPanel detailRingkasanMatkulGUI = new JPanel();
+        // Menambahkan glue agar konten ada di tengah
+        detailRingkasanMatkulGUI.add(Box.createVerticalGlue());
 
-        // TODO: Implementasikan Detail Ringkasan Mata Kuliah
-        
+        detailRingkasanMatkulGUI.setLayout(new BoxLayout(detailRingkasanMatkulGUI, BoxLayout.Y_AXIS));
+        detailRingkasanMatkulGUI.setBackground(new Color(0xCD7841));
+
+        // Menambahkan label judul
+        JLabel newLabel = new JLabel();
+        newLabel.setText("Detail Ringkasan Mata Kuliah");
+        newLabel.setFont(SistemAkademikGUI.fontTitle);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        detailRingkasanMatkulGUI.add(newLabel);
+        detailRingkasanMatkulGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menampung variabel label-label yang perlu ditampung
+        String nama = mataKuliah.getNama();
+        String kode = mataKuliah.getKode();
+        int sks = mataKuliah.getSKS();
+        int jumlahMahasiswa = mataKuliah.getJumlahMahasiswa();
+        int kapasitas = mataKuliah.getKapasitas();
+        Mahasiswa[] daftarMahasiswaMatkul = mataKuliah.getDaftarMahasiswa();
+
+        // Menambahkan label-label yang akan ditampilkan
+        addLabelGeneral(detailRingkasanMatkulGUI, "Nama mata kuliah: " + nama);
+        addLabelGeneral(detailRingkasanMatkulGUI, "Kode: " + kode);
+        addLabelGeneral(detailRingkasanMatkulGUI, "SKS: " + sks);
+        addLabelGeneral(detailRingkasanMatkulGUI, "Jumlah mahasiswa: " + jumlahMahasiswa);
+        addLabelGeneral(detailRingkasanMatkulGUI, "Kapasitas: " + kapasitas);
+        addLabelGeneral(detailRingkasanMatkulGUI, "Daftar Mahasiswa:");
+
+        // Mencetak daftar mahasiswa yang mengambil matkul yang bersangkutan
+        if (jumlahMahasiswa == 0) {
+            JLabel daftarMahasiswaLabel = new JLabel();
+            daftarMahasiswaLabel.setText("Belum ada mahasiswa yang mengambil mata kuliah ini.");
+            daftarMahasiswaLabel.setFont(new Font("Century Gothic", Font.BOLD, 12));
+            daftarMahasiswaLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+            detailRingkasanMatkulGUI.add(daftarMahasiswaLabel);
+            detailRingkasanMatkulGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+        } else {
+            for (int i = 0; i < jumlahMahasiswa; i++) {
+                JLabel daftarMahasiswaLabel = new JLabel();
+                daftarMahasiswaLabel.setText(Integer.toString(i + 1) + ". " + daftarMahasiswaMatkul[i].getNama());
+                daftarMahasiswaLabel.setFont(new Font("Century Gothic", Font.BOLD, 12));
+                daftarMahasiswaLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                detailRingkasanMatkulGUI.add(daftarMahasiswaLabel);
+                detailRingkasanMatkulGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+
+        // Menambahkan button Selesai
+        JButton kembaliButton = new JButton("Selesai");
+        kembaliButton.setFocusable(false);
+        kembaliButton.setFont(SistemAkademikGUI.fontGeneral);
+        kembaliButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        kembaliButton.setForeground(new Color(0xFFFFFF));
+        kembaliButton.setBackground(new Color(0x059846));
+        kembaliButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Menghapus panel
+                frame.getContentPane().removeAll();
+                frame.repaint();
+
+                // Pindah ke panel homeGUI
+                new HomeGUI(frame, daftarMahasiswa, daftarMataKuliah);
+            }
+        });
+        detailRingkasanMatkulGUI.add(kembaliButton);
+
+        // Menambahkan glue agar konten ada di tengah
+        detailRingkasanMatkulGUI.add(Box.createVerticalGlue());
+
+        frame.add(detailRingkasanMatkulGUI);
+        frame.setVisible(true);
+
     }
+
+    private void addLabelGeneral(JPanel panel, String text) {
+        JLabel newLabel = new JLabel();
+        newLabel.setText(text);
+        newLabel.setFont(SistemAkademikGUI.fontGeneral);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(newLabel);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+    }
+
 }
+
--- assignment4/src/main/java/assignments/assignment4/frontend/RingkasanMataKuliahGUI.java
+++ assignment4/src/main/java/assignments/assignment4/frontend/RingkasanMataKuliahGUI.java
@@ -10,14 +10,124 @@ import assignments.assignment4.backend.*;
 public class RingkasanMataKuliahGUI {
 
     public RingkasanMataKuliahGUI(JFrame frame, ArrayList<Mahasiswa> daftarMahasiswa, ArrayList<MataKuliah> daftarMataKuliah){
+        JPanel ringkasanMataKuliahGUI = new JPanel();
+        // Menambahkan glue agar konten ada di tengah
+        ringkasanMataKuliahGUI.add(Box.createVerticalGlue());
 
-        // TODO: Implementasikan Ringkasan Mata Kuliah
-        
+        ringkasanMataKuliahGUI.setLayout(new BoxLayout(ringkasanMataKuliahGUI, BoxLayout.Y_AXIS));
+        ringkasanMataKuliahGUI.setBackground(new Color(0xCD7841));
+
+        // Membuat array yang menyimpan nama mata kuliah untuk ditampilkan pada dropdown
+        String[] arrayNamaMatkul = new String[daftarMataKuliah.size()];
+
+        // Mengisi array arrayNamaMatkul dengan nama mata kuliah dari setiap mata kuliah
+        for (int i = 0; i < daftarMataKuliah.size(); i++) {
+            if (daftarMataKuliah.get(i) != null) {
+                arrayNamaMatkul[i] = daftarMataKuliah.get(i).getNama();
+            } else if (daftarMataKuliah.get(i) == null) {
+                break;
+            }
+        }
+
+        // Melakukan sort terhadap arrayNpm
+        String[] arrayNamaMatkulSorted = sortNamaMatkul(arrayNamaMatkul);
+
+        // Menambahkan label judul
+        JLabel newLabel = new JLabel();
+        newLabel.setText("Ringkasan Mata Kuliah");
+        newLabel.setFont(SistemAkademikGUI.fontTitle);
+        newLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ringkasanMataKuliahGUI.add(newLabel);
+        ringkasanMataKuliahGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan label Pilih NPM
+        JLabel pilihNamaMatkulLabel = new JLabel();
+        pilihNamaMatkulLabel.setText("Pilih Nama Matkul");
+        pilihNamaMatkulLabel.setFont(SistemAkademikGUI.fontGeneral);
+        pilihNamaMatkulLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ringkasanMataKuliahGUI.add(pilihNamaMatkulLabel);
+        ringkasanMataKuliahGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan dropdownMataKuliah
+        JComboBox dropdownMataKuliah = new JComboBox(arrayNamaMatkulSorted);
+        dropdownMataKuliah.setMaximumSize(new Dimension(175, 0));
+        ringkasanMataKuliahGUI.add(dropdownMataKuliah);
+        ringkasanMataKuliahGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan button Lihat
+        JButton lihatButton = new JButton("lihat");
+        lihatButton.setFocusable(false);
+        lihatButton.setFont(SistemAkademikGUI.fontGeneral);
+        lihatButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        lihatButton.setForeground(new Color(0xFFFFFF));
+        lihatButton.setBackground(new Color(0x0549B0));
+        lihatButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Mengambil nilai dari dropdown
+                String namaMatkul =  (String) dropdownMataKuliah.getSelectedItem();
+
+                // Jika terdapat dropdown yang masih kosong
+                if (namaMatkul == null) {
+                    JOptionPane.showMessageDialog(frame, "Mohon isi seluruh Field");
+                } else {
+                    // Membuka panel detail ringkasan mata kuliah
+                    MataKuliah mataKuliah = getMataKuliah(namaMatkul, daftarMataKuliah);
+
+                    // Menghapus panel
+                    frame.getContentPane().removeAll();
+                    frame.repaint();
+
+                    new DetailRingkasanMataKuliahGUI(frame, mataKuliah, daftarMahasiswa, daftarMataKuliah);
+                }
+            }
+        });
+        ringkasanMataKuliahGUI.add(lihatButton);
+        ringkasanMataKuliahGUI.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Menambahkan button Kembali
+        JButton kembaliButton = new JButton("Kembali");
+        kembaliButton.setFocusable(false);
+        kembaliButton.setFont(SistemAkademikGUI.fontGeneral);
+        kembaliButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        kembaliButton.setForeground(new Color(0xFFFFFF));
+        kembaliButton.setBackground(new Color(0x059846));
+        kembaliButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Menghapus panel
+                frame.getContentPane().removeAll();
+                frame.repaint();
+
+                // Pindah ke panel homeGUI
+                new HomeGUI(frame, daftarMahasiswa, daftarMataKuliah);
+            }
+        });
+        ringkasanMataKuliahGUI.add(kembaliButton);
+
+
+        // Menambahkan glue agar konten ada di tengah
+        ringkasanMataKuliahGUI.add(Box.createVerticalGlue());
+
+        frame.add(ringkasanMataKuliahGUI);
+        frame.setVisible(true);
     }
 
-    // Uncomment method di bawah jika diperlukan
-    /*
-    private MataKuliah getMataKuliah(String nama) {
+    private String[] sortNamaMatkul(String[] namaMatkul) {
+        for (int i = 0; i < namaMatkul.length; i++) {
+            for (int j = i + 1; j < namaMatkul.length; j++) {
+                if (namaMatkul[i].compareToIgnoreCase(namaMatkul[j]) > 0) {
+                    // Melakukan swap
+                    String tempMin = namaMatkul[j];
+                    namaMatkul[j] = namaMatkul[i];
+                    namaMatkul[i] = tempMin;
+                }
+            }
+        }
+        return namaMatkul;
+    }
+
+    private MataKuliah getMataKuliah(String nama, ArrayList<MataKuliah> daftarMataKuliah) {
 
         for (MataKuliah mataKuliah : daftarMataKuliah) {
             if (mataKuliah.getNama().equals(nama)){
@@ -26,5 +136,5 @@ public class RingkasanMataKuliahGUI {
         }
         return null;
     }
-    */
+
 }
